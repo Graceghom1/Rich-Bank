7cadd8888e871934fbcaea127b8db7e01aba6833
FullStackDev97 <emiliosensuavictor@gmail.com>
2024-01-16 16:27:32 +0100

    adding tailwind for page styling

--- My-AppAngular/src/app/app.component.ts
+++ My-AppAngular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My-AppAngular';
+  title = 'Rich Bank';
 }
--- My-AppAngular/src/app/app.module.ts
+++ My-AppAngular/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import  {AuthPageComponent} from './auth-page/auth-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
